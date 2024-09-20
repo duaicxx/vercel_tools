ff54aa2ba89b6cedd7055f9e1c20c6ae8e7e7d4d
starskye <1582293004@qq.com>
2024-09-20 11:59:01 +0800

    浏览器

---
+++ pages/api/puppeteer/index.js
@@ -0,0 +1,25 @@
+import runMiddleware from "../middleware/middleware";
+import {
+  cors,
+  ERROR_MESSAGE,
+} from "../utils/const";
+import { puppeteerOpenBrowser } from "../utils/utils";
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
+  try {
+    //get query params
+    const { url } = req.query;
+
+    //make request
+    const { $ } = await puppeteerOpenBrowser(
+      url
+    );
+    return res.json({
+      status: 200,
+      content: $.html()
+    });
+  } catch {
+    res.status(404).json({ status: 404, error: ERROR_MESSAGE });
+  }
+}
